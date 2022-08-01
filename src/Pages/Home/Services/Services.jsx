a19d3b7492ecdd9e50e6bbcf50eec92a97265359
Shorif4321 <shorifali4321@gmail.com>
2022-08-01 14:58:40 +0600

    service

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./Services.css"
 import img1 from "../../../Images/images.png"
-
+import TwentyTwenty from "react-twentytwenty";
 
 
 const Services = () => {
@@ -13,7 +13,6 @@ const Services = () => {
 
 
     const [services, setServices] = useState([]);
-    /*     import TwentyTwenty from "react-twentytwenty"; */
 
     useEffect(() => {
         fetch('./Services.JSON')
@@ -28,33 +27,32 @@ const Services = () => {
 
     return (
         <div className=''>
-            <div className="container service-section">
-
-                {/* <TwentyTwenty
-                    left={<img src={img1} />}
-                    right={<img src="images/andew.jpg" alt="Andrew" />}
-                />
- */}
-
-
-
-
-                <h2 className='fs-1 text-bold mb-3'>
-                    Services We Provide
-                </h2>
-                <p className='my-4 common-p'>Clipping Path Studio – An outsourcing company providing all kinds of image editing services including background removal, image masking, ghost mannequin effect, shadow creation, color correction, and eCommerce Product photo editing service.</p>
+            <div className="container service-section mb-5">
+                <div className='col-10 m-auto'>
+                    <h2 className='fs-1 text-bold mb-3'>
+                        Services We Provide
+                    </h2>
+                    <p className='my-4 common-p'>Clipping Path Studio – An outsourcing company providing all kinds of image editing services including background removal, image masking, ghost mannequin effect, shadow creation, color correction, and eCommerce Product photo editing service.</p>
+                </div>
 
                 <div className="row ">
                     {
-                        services?.map(service => <div className="col-6 g-4 my-3">
+                        services?.map(service => <div className="col-12 col-md-6 g-4 my-3">
                             <div className="clipping-main p-4">
-                                <div>
-                                    <img src={service?.image} className="card-img-top" alt="..." />
+                                <div className='col-12 col-md-5'>
+                                    <TwentyTwenty
+                                        left={<img className="card-img-top" src={service?.image} />}
+                                        right={<img className="card-img-top" src={service?.image2} />}
+                                        slider={<div className="slider" />}
+
+                                    />
+
+
                                 </div>
-                                <div className="card-body text-start ps-4 ">
+                                <div className="card-body text-start ps-md-4 col-12 col-md-7">
                                     <h3 className="card-title">{service.name}</h3>
                                     <p className="card-text py-3 common-p">{service.des.slice(0, 325)}</p>
-                                    <Link className='service-button' to="/">Explore</Link>
+                                    <Link className='service-button text-center' to="/">Explore</Link>
                                 </div>
                             </div>
                         </div>
