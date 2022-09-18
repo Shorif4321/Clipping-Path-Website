91d97249e0dc75a5efa05a9c4bc4c276eca25389
Shorif4321 <shorifali4321@gmail.com>
2022-09-18 11:43:59 +0600

    finish

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
         <div>
             <section>
                 <div className="d-md-flex align-items-center mt-5 container">
-                    <div className="col-12 col-md-7">
+                    <div className="col-12 col-md-7 ">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d233668.38703802988!2d90.27923923029098!3d23.780573257422205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1656134714332!5m2!1sen!2sbd" style={{ width: "100%", height: '400px' }}></iframe>
                     </div>
                     <div className="map-left col-12  col-md-5  ms-md-4">
